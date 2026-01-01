3088387c5d216c44ccf7a0d5538943a6c63188dd
Darío Hernández Cubilla <dariohcvg@gmail.com>
2026-01-01 14:08:48 -0500

    feat: add auto ML setup scaffolding

---
+++ setup/evaluator/setup.py
@@ -0,0 +1,6 @@
+from auto_ml.implementations.nodes import EvaluatorNode
+
+
+def get_evaluator_node() -> EvaluatorNode:
+    """Return evaluator node for use in Auto-ML."""
+    raise NotImplementedError()
---
+++ setup/models/setup.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from auto_ml.implementations.nodes import ModelNode
+
+
+def get_model_nodes() -> List[ModelNode]:
+    """Return a list of model nodes for use in Auto-ML."""
+    raise NotImplementedError()
